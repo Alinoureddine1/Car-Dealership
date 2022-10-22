dcb794b0d531fe1aa748408674bed27493298c43
Ali <66390544+Alinoureddine1@users.noreply.github.com>
2022-10-21 21:48:29 -0400

    Create UsedCar.cpp

---
+++ UsedCar.cpp
@@ -0,0 +1,33 @@
+#include "UsedCar.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+UsedCar::UsedCar() :NewCar() {
+	this->manifacturer = "";
+	this->milage = 0;
+}
+UsedCar::UsedCar(int id, int year, std::string color, int doors, double prix, std::string manifac , int miles) :NewCar( id, year, color, doors, prix) {
+	this->manifacturer = manifac;
+	this->milage = miles;
+}
+void UsedCar::print() {
+	NewCar::print();
+	cout <<"Manifacturer: "<< this->manifacturer << endl;
+	cout << "Milage: " << this->milage << " miles" << endl;
+}
+void UsedCar::setmanifacturer(std::string manifac) {
+	this->manifacturer = manifac;
+}
+void UsedCar::setmilage(int miles) {
+	this->milage = miles;
+}
+std::string UsedCar::getmanifacturer() {
+	return this->manifacturer;
+}
+int UsedCar::getmilage() {
+	return this->milage;
+}
+UsedCar::~UsedCar() {
+	cout << "Object of type UsedCard was deleted" << endl;
+}
