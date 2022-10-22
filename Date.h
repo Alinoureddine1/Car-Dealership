465ca49af9500799f3b8e045eb10cb46e5ceee8a
Ali <66390544+Alinoureddine1@users.noreply.github.com>
2022-10-21 21:46:16 -0400

    Create Date.h

---
+++ Date.h
@@ -0,0 +1,21 @@
+#include <string>
+
+class Date
+{
+private:
+	int day;
+	int month;
+	int year;
+public:
+	Date();
+	Date(int, int, int);
+	~Date();
+	void setday(int);
+	void setmonth(int);
+	void setyear(int);
+	int getday();
+	int getmonth();
+	int getyear();
+	void print();
+
+};
