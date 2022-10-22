cf53da048aed56edd1139e3842d19cffee1e4518
Ali <66390544+Alinoureddine1@users.noreply.github.com>
2022-10-21 21:45:53 -0400

    Update Driver.cpp

--- Driver.cpp
+++ Driver.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include "Car.h"
+#include "Date.h"
+#include "NewCar.h"
+#include "UsedCar.h"
+using namespace std;
+int main()
+{
+    NewCar car(122, 2012, "Pink", 2, 2300);
+    car.print();
+
+
+    return 0;
+}
 
