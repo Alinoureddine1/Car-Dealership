0f8b131c9cb0e0f935786149e6ce9cb971d3aa04
Ali <66390544+Alinoureddine1@users.noreply.github.com>
2022-10-21 21:46:37 -0400

    Create Date.cpp

---
+++ Date.cpp
@@ -0,0 +1,45 @@
+
+#include "Date.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+Date::Date() {
+	this->day = 0;
+	this->month = 0;
+	this->year = 0;
+}
+
+Date::Date(int x,int y, int z) {
+	this->day = x;
+	this->month = y;
+	this->year = z;
+
+}
+
+Date::~Date() {
+	cout << "Object of type Date was deleted \n";
+}
+
+void Date::print() {
+	cout <<"Date: " << this->month << "/" << this->day << "/" << this->year << endl;
+}
+
+void Date::setday(int x) {
+	this->day = x;
+}
+void Date::setmonth(int y) {
+	this->month = y;
+}
+void Date::setyear(int z) {
+	this->year = z;
+}
+int Date::getday() {
+	return this->day;
+}
+int Date::getmonth() {
+	return this->month;
+}
+int Date::getyear() {
+	return this->year;
+}
